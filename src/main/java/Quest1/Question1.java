b7d3940176582bc4d98486c4d99c4e29dc7924a1
Yakimov Mihail <mihail.yakimov@list.ru>
2023-10-25 22:01:34 +0400

    commit

---
+++ src/main/java/Quest1/Question1.java
@@ -0,0 +1,12 @@
+package Quest1;
+
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        double a = 4;
+        double area = Math.pow(a, 2);
+        System.out.println("Площадь квадрата равна: " + area);
+
+    }
+}
---
+++ src/main/java/Quest2/Question2.java
@@ -0,0 +1,12 @@
+package Quest2;
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        double r = 5;
+        double area =Math.PI * Math.pow(r,2);
+        System.out.println("Площадь круга равна: "+ area);
+
+        }
+    }
